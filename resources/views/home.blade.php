0f045910a37ab218bba13bd5df7d9c1b44922094
saifal56 <103521997+saifal56@users.noreply.github.com>
2022-11-11 12:16:19 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume/CV Design</title>
+	<link rel="stylesheet" href="{{asset('stylescv.css')}}">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="Saifal Asna kesekretariatan  (1).jpg" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">Saifal Asna</p>
+           <p class="regular">Web Designer | Youtuber</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Aceh Besar <br /> Indonesia
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               +6285285857922
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               saifalasna682@gmail.com
+             </div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skill's</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               HTML
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               CSS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Corel
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               JS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               PHP
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+          <li>
+            <li>      
+            <div class="skill_name">
+                    Indonesia
+                  </div>
+                  <div class="skill_progress">
+                    <span style="width: 80%;"></span>
+                  </div>
+                  <div class="skill_per">80%</div>
+                </li>
+              <li>
+                <div class="skill_name">
+                  English
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 70%;"></span>
+                </div>
+                <div class="skill_per">70%</div>
+              </li>
+              <li>
+                <div class="skill_name">
+                  Aceh
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 100%;"></span>
+                </div>
+                <div class="skill_per">100%</div>
+              </li>
+          </li>
+        </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About us</p>
+         </div>
+        <p>My Name is Saifal Asna, I'm 22 Years Old, I From indrapuri, Aceh Besar, Indonesia</p>
+        <p>I Graduated from state islamic University of AR-RANIRY From Information Technology</p>
+        <p>My Hoby is Playing Games, Motorcycle ride, and climb</p>
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Education</p>
+         </div>
+        <ul>
+            <li>
+                <div class="date">2003-2009</div> 
+                <div class="info">
+                     <p class="semi-bold">MIN indrapuri</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2009-2012</div>
+              <div class="info">
+                     <p class="semi-bold">MTsN Oemar Diyan</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2012 - 2015</div>
+              <div class="info">
+                     <p class="semi-bold">SMKN SMTI Banda Aceh</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2015 - 2020</div>
+              <div class="info">
+                     <p class="semi-bold">State Islamic University of AR-RANIRY, Banda Aceh</p> 
+                  <p> Information Technology</p>
+                </div>
+            </li>
+            
+          </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Work Experience</p>
+         </div>
+      <ul>
+            
+            <li>
+              <div class="date">2015 - 2022</div>
+              <div class="info">
+                <p class="semi-bold">work in the workshop</p> 
+                  
+                </div>
+            </li>
+           
+        </ul>
+    </div>
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class="fas fa-book"></i></li>
+         <li><i class="fas fa-gamepad"></i></li>
+         <li><i class="fas fa-motorcycle"></i></li>
+         <li><i class="fas fa-mountain"></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
